bb33b07809bc564aaed9da5e9c78157dde9f6624
Marcin Wawrzonowski <MaverickCCCP@Gmail.com>
2019-05-09 00:57:19 +0200

    [FTPS] Push script prompt check.

--- FTPSync/scripts/FTPSync_push.bat
+++ FTPSync/scripts/FTPSync_push.bat
@@ -1,6 +1,11 @@
 @echo off
 setlocal
 
-call FTPSync.bat PUSH
+set /p yes="Are you sure you want to perform PUSH operation? You may accidentally delete or mess up something! [Y/N]: "
+
+if %yes% == y call FTPSync.bat PUSH
+if %yes% == Y call FTPSync.bat PUSH
+
+:eof
 
 endlocal
