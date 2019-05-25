6d241e467d4aad6220ed54d87e379a30a6680fa0
Marcin Wawrzonowski <MaverickCCCP@Gmail.com>
2019-05-25 17:48:39 +0200

    [AC] AC project stub added.

---
+++ AC/src/defs.h
@@ -0,0 +1,8 @@
+// Global definition file that should be included in every .c file.
+
+#define CPU_FREQ 8000000UL
+#define BAUD_RATE 9600
+
+#define bool unsigned char
+#define true 1
+#define false 0
---
+++ AC/src/main.c
@@ -0,0 +1,8 @@
+#include "defs.h"
+
+int main(void)
+{
+    while(true) ;
+
+    return 0;   // Should never get here.
+}
